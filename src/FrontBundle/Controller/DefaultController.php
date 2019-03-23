2c0c81a5a3790faebee9f214593626c313ed7693
akthemarfaoui <47156414+akthemarfaoui@users.noreply.github.com>
2019-03-24 00:42:32 +0100

    Frontend intégré #Akthem !

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrontBundle:Default:index.html.twig');
+        return $this->render('@Front/Default/index.html.twig');
+    }
+    public function layoutAction()
+    {
+        return $this->render('@Front/layout.html.twig');
     }
 }
